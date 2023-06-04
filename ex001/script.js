e07956f6f436f1d054e006d8b29a055f0a1d7a11
DiogoSilveira10 <134713530+DiogoSilveira10@users.noreply.github.com>
2023-06-04 11:00:53 -0300

    arquivos sobre javascript

---
+++ ex001/script.js
@@ -0,0 +1,14 @@
+// let ver = 1
+// document.getElementById('btn').onclick = function(){
+
+//     if (ver == 1){
+//         document.getElementById('Hum'). innerHTML = "Eu modifiquei esse titulo utilizando javascript e com o comando document.getElementById('Hum').innerHTML = *esse texto* "
+//         ver = 0
+//     }
+//     else{
+//         document.getElementById('Hum'). innerHTML = "JavaScript"
+//         ver = 1
+//     }
+// }
+
+
