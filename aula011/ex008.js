bee55ab5c6c514ce55bd69844b1146fcc1ff6d75
DiogoSilveira10 <134713530+DiogoSilveira10@users.noreply.github.com>
2023-06-05 17:06:54 -0300

    arquivos adicionados

---
+++ aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel = 59
+console.log('A velocidade de vel é ' + vel)
+
+if(vel >= 60){
+    console.log('a sua velocidade esta acima da velocidade permitida. ')
+}
+
+console.log('diriga sempre com cinto de seguranca')
